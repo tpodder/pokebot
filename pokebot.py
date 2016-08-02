5d65a0204c595cc41eba602441671fd328988b5f
Tanushree Podder <tanushree.podder.103@gmail.com>
2016-08-01 21:34:27 -0400

    Pokebot Version 1.0

---
+++ pokebot.py
@@ -0,0 +1,975 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+import sys
+import json
+import time
+import struct
+import random
+import logging
+import requests
+import argparse
+import pprint
+import pykemon
+
+from pgoapi import PGoApi
+from pgoapi.utilities import f2i, h2f
+from pgoapi import utilities as util
+
+from google.protobuf.internal import encoder
+from geopy.geocoders import GoogleV3
+from s2sphere import Cell, CellId, LatLng
+
+import os
+import time
+from slackclient import SlackClient
+    
+    
+    
+
+log = logging.getLogger(__name__)
+
+import os
+import time
+
+pokemonNames =  ["Bulbasaur",
+	"Ivysaur",
+	"Venusaur",
+	"Charmander",
+	"Charmeleon",
+	"Charizard",
+	"Squirtle",
+	"Wartortle",
+	"Blastoise",
+	"Caterpie",
+	"Metapod",
+	"Butterfree",
+	"Weedle",
+	"Kakuna",
+	"Beedrill",
+	"Pidgey",
+	"Pidgeotto",
+	"Pidgeot",
+	"Rattata",
+	"Raticate",
+	"Spearow",
+	"Fearow",
+	"Ekans",
+	"Arbok",
+	"Pikachu",
+	"Raichu",
+	"Sandshrew",
+	"Sandslash",
+	"Nidoran♀",
+	"Nidorina",
+	"Nidoqueen",
+	"Nidoran♂",
+	"Nidorino",
+	"Nidoking",
+	"Clefairy",
+	"Clefable",
+	"Vulpix",
+	"Ninetales",
+	"Jigglypuff",
+	"Wigglytuff",
+	"Zubat",
+	"Golbat",
+	"Oddish",
+	"Gloom",
+	"Vileplume",
+	"Paras",
+	"Parasect",
+	"Venonat",
+	"Venomoth",
+	"Diglett",
+	"Dugtrio",
+	"Meowth",
+	"Persian",
+	"Psyduck",
+	"Golduck",
+	"Mankey",
+	"Primeape",
+	"Growlithe",
+	"Arcanine",
+	"Poliwag",
+	"Poliwhirl",
+	"Poliwrath",
+	"Abra",
+	"Kadabra",
+	"Alakazam",
+	"Machop",
+	"Machoke",
+	"Machamp",
+	"Bellsprout",
+	"Weepinbell",
+	"Victreebel",
+	"Tentacool",
+	"Tentacruel",
+	"Geodude",
+	"Graveler",
+	"Golem",
+	"Ponyta",
+	"Rapidash",
+	"Slowpoke",
+	"Slowbro",
+	"Magnemite",
+	"Magneton",
+	"Farfetch’d",
+	"Doduo",
+	"Dodrio",
+	"Seel",
+	"Dewgong",
+	"Grimer",
+	"Muk",
+	"Shellder",
+	"Cloyster",
+	"Gastly",
+	"Haunter",
+	"Gengar",
+	"Onix",
+	"Drowzee",
+	"Hypno",
+	"Krabby",
+	"Kingler",
+	"Voltorb",
+	"Electrode",
+	"Exeggcute",
+	"Exeggutor",
+	"Cubone",
+	"Marowak",
+	"Hitmonlee",
+	"Hitmonchan",
+	"Lickitung",
+	"Koffing",
+	"Weezing",
+	"Rhyhorn",
+	"Rhydon",
+	"Chansey",
+	"Tangela",
+	"Kangaskhan",
+	"Horsea",
+	"Seadra",
+	"Goldeen",
+	"Seaking",
+	"Staryu",
+	"Starmie",
+	"Mr. Mime",
+	"Scyther",
+	"Jynx",
+	"Electabuzz",
+	"Magmar",
+	"Pinsir",
+	"Tauros",
+	"Magikarp",
+	"Gyarados",
+	"Lapras",
+	"Ditto",
+	"Eevee",
+	"Vaporeon",
+	"Jolteon",
+	"Flareon",
+	"Porygon",
+	"Omanyte",
+	"Omastar",
+	"Kabuto",
+	"Kabutops",
+	"Aerodactyl",
+	"Snorlax",
+	"Articuno",
+	"Zapdos",
+	"Moltres",
+	"Dratini",
+	"Dragonair",
+	"Dragonite",
+	"Mewtwo",
+	"Mew",
+	"Chikorita",
+	"Bayleef",
+	"Meganium",
+	"Cyndaquil",
+	"Quilava",
+	"Typhlosion",
+	"Totodile",
+	"Croconaw",
+	"Feraligatr",
+	"Sentret",
+	"Furret",
+	"Hoothoot",
+	"Noctowl",
+	"Ledyba",
+	"Ledian",
+	"Spinarak",
+	"Ariados",
+	"Crobat",
+	"Chinchou",
+	"Lanturn",
+	"Pichu",
+	"Cleffa",
+	"Igglybuff",
+	"Togepi",
+	"Togetic",
+	"Natu",
+	"Xatu",
+	"Mareep",
+	"Flaaffy",
+	"Ampharos",
+	"Bellossom",
+	"Marill",
+	"Azumarill",
+	"Sudowoodo",
+	"Politoed",
+	"Hoppip",
+	"Skiploom",
+	"Jumpluff",
+	"Aipom",
+	"Sunkern",
+	"Sunflora",
+	"Yanma",
+	"Wooper",
+	"Quagsire",
+	"Espeon",
+	"Umbreon",
+	"Murkrow",
+	"Slowking",
+	"Misdreavus",
+	"Unown",
+	"Wobbuffet",
+	"Girafarig",
+	"Pineco",
+	"Forretress",
+	"Dunsparce",
+	"Gligar",
+	"Steelix",
+	"Snubbull",
+	"Granbull",
+	"Qwilfish",
+	"Scizor",
+	"Shuckle",
+	"Heracross",
+	"Sneasel",
+	"Teddiursa",
+	"Ursaring",
+	"Slugma",
+	"Magcargo",
+	"Swinub",
+	"Piloswine",
+	"Corsola",
+	"Remoraid",
+	"Octillery",
+	"Delibird",
+	"Mantine",
+	"Skarmory",
+	"Houndour",
+	"Houndoom",
+	"Kingdra",
+	"Phanpy",
+	"Donphan",
+	"Porygon2",
+	"Stantler",
+	"Smeargle",
+	"Tyrogue",
+	"Hitmontop",
+	"Smoochum",
+	"Elekid",
+	"Magby",
+	"Miltank",
+	"Blissey",
+	"Raikou",
+	"Entei",
+	"Suicune",
+	"Larvitar",
+	"Pupitar",
+	"Tyranitar",
+	"Lugia",
+	"Ho-Oh",
+	"Celebi",
+	"Treecko",
+	"Grovyle",
+	"Sceptile",
+	"Torchic",
+	"Combusken",
+	"Blaziken",
+	"Mudkip",
+	"Marshtomp",
+	"Swampert",
+	"Poochyena",
+	"Mightyena",
+	"Zigzagoon",
+	"Linoone",
+	"Wurmple",
+	"Silcoon",
+	"Beautifly",
+	"Cascoon",
+	"Dustox",
+	"Lotad",
+	"Lombre",
+	"Ludicolo",
+	"Seedot",
+	"Nuzleaf",
+	"Shiftry",
+	"Taillow",
+	"Swellow",
+	"Wingull",
+	"Pelipper",
+	"Ralts",
+	"Kirlia",
+	"Gardevoir",
+	"Surskit",
+	"Masquerain",
+	"Shroomish",
+	"Breloom",
+	"Slakoth",
+	"Vigoroth",
+	"Slaking",
+	"Nincada",
+	"Ninjask",
+	"Shedinja",
+	"Whismur",
+	"Loudred",
+	"Exploud",
+	"Makuhita",
+	"Hariyama",
+	"Azurill",
+	"Nosepass",
+	"Skitty",
+	"Delcatty",
+	"Sableye",
+	"Mawile",
+	"Aron",
+	"Lairon",
+	"Aggron",
+	"Meditite",
+	"Medicham",
+	"Electrike",
+	"Manectric",
+	"Plusle",
+	"Minun",
+	"Volbeat",
+	"Illumise",
+	"Roselia",
+	"Gulpin",
+	"Swalot",
+	"Carvanha",
+	"Sharpedo",
+	"Wailmer",
+	"Wailord",
+	"Numel",
+	"Camerupt",
+	"Torkoal",
+	"Spoink",
+	"Grumpig",
+	"Spinda",
+	"Trapinch",
+	"Vibrava",
+	"Flygon",
+	"Cacnea",
+	"Cacturne",
+	"Swablu",
+	"Altaria",
+	"Zangoose",
+	"Seviper",
+	"Lunatone",
+	"Solrock",
+	"Barboach",
+	"Whiscash",
+	"Corphish",
+	"Crawdaunt",
+	"Baltoy",
+	"Claydol",
+	"Lileep",
+	"Cradily",
+	"Anorith",
+	"Armaldo",
+	"Feebas",
+	"Milotic",
+	"Castform",
+	"Kecleon",
+	"Shuppet",
+	"Banette",
+	"Duskull",
+	"Dusclops",
+	"Tropius",
+	"Chimecho",
+	"Absol",
+	"Wynaut",
+	"Snorunt",
+	"Glalie",
+	"Spheal",
+	"Sealeo",
+	"Walrein",
+	"Clamperl",
+	"Huntail",
+	"Gorebyss",
+	"Relicanth",
+	"Luvdisc",
+	"Bagon",
+	"Shelgon",
+	"Salamence",
+	"Beldum",
+	"Metang",
+	"Metagross",
+	"Regirock",
+	"Regice",
+	"Registeel",
+	"Latias",
+	"Latios",
+	"Kyogre",
+	"Groudon",
+	"Rayquaza",
+	"Jirachi",
+	"Deoxys",
+	"Turtwig",
+	"Grotle",
+	"Torterra",
+	"Chimchar",
+	"Monferno",
+	"Infernape",
+	"Piplup",
+	"Prinplup",
+	"Empoleon",
+	"Starly",
+	"Staravia",
+	"Staraptor",
+	"Bidoof",
+	"Bibarel",
+	"Kricketot",
+	"Kricketune",
+	"Shinx",
+	"Luxio",
+	"Luxray",
+	"Budew",
+	"Roserade",
+	"Cranidos",
+	"Rampardos",
+	"Shieldon",
+	"Bastiodon",
+	"Burmy",
+	"Wormadam",
+	"Mothim",
+	"Combee",
+	"Vespiquen",
+	"Pachirisu",
+	"Buizel",
+	"Floatzel",
+	"Cherubi",
+	"Cherrim",
+	"Shellos",
+	"Gastrodon",
+	"Ambipom",
+	"Drifloon",
+	"Drifblim",
+	"Buneary",
+	"Lopunny",
+	"Mismagius",
+	"Honchkrow",
+	"Glameow",
+	"Purugly",
+	"Chingling",
+	"Stunky",
+	"Skuntank",
+	"Bronzor",
+	"Bronzong",
+	"Bonsly",
+	"Mime Jr.",
+	"Happiny",
+	"Chatot",
+	"Spiritomb",
+	"Gible",
+	"Gabite",
+	"Garchomp",
+	"Munchlax",
+	"Riolu",
+	"Lucario",
+	"Hippopotas",
+	"Hippowdon",
+	"Skorupi",
+	"Drapion",
+	"Croagunk",
+	"Toxicroak",
+	"Carnivine",
+	"Finneon",
+	"Lumineon",
+	"Mantyke",
+	"Snover",
+	"Abomasnow",
+	"Weavile",
+	"Magnezone",
+	"Lickilicky",
+	"Rhyperior",
+	"Tangrowth",
+	"Electivire",
+	"Magmortar",
+	"Togekiss",
+	"Yanmega",
+	"Leafeon",
+	"Glaceon",
+	"Gliscor",
+	"Mamoswine",
+	"Porygon-Z",
+	"Gallade",
+	"Probopass",
+	"Dusknoir",
+	"Froslass",
+	"Rotom",
+	"Uxie",
+	"Mesprit",
+	"Azelf",
+	"Dialga",
+	"Palkia",
+	"Heatran",
+	"Regigigas",
+	"Giratina",
+	"Cresselia",
+	"Phione",
+	"Manaphy",
+	"Darkrai",
+	"Shaymin",
+	"Arceus",
+	"Victini",
+	"Snivy",
+	"Servine",
+	"Serperior",
+	"Tepig",
+	"Pignite",
+	"Emboar",
+	"Oshawott",
+	"Dewott",
+	"Samurott",
+	"Patrat",
+	"Watchog",
+	"Lillipup",
+	"Herdier",
+	"Stoutland",
+	"Purrloin",
+	"Liepard",
+	"Pansage",
+	"Simisage",
+	"Pansear",
+	"Simisear",
+	"Panpour",
+	"Simipour",
+	"Munna",
+	"Musharna",
+	"Pidove",
+	"Tranquill",
+	"Unfezant",
+	"Blitzle",
+	"Zebstrika",
+	"Roggenrola",
+	"Boldore",
+	"Gigalith",
+	"Woobat",
+	"Swoobat",
+	"Drilbur",
+	"Excadrill",
+	"Audino",
+	"Timburr",
+	"Gurdurr",
+	"Conkeldurr",
+	"Tympole",
+	"Palpitoad",
+	"Seismitoad",
+	"Throh",
+	"Sawk",
+	"Sewaddle",
+	"Swadloon",
+	"Leavanny",
+	"Venipede",
+	"Whirlipede",
+	"Scolipede",
+	"Cottonee",
+	"Whimsicott",
+	"Petilil",
+	"Lilligant",
+	"Basculin",
+	"Sandile",
+	"Krokorok",
+	"Krookodile",
+	"Darumaka",
+	"Darmanitan",
+	"Maractus",
+	"Dwebble",
+	"Crustle",
+	"Scraggy",
+	"Scrafty",
+	"Sigilyph",
+	"Yamask",
+	"Cofagrigus",
+	"Tirtouga",
+	"Carracosta",
+	"Archen",
+	"Archeops",
+	"Trubbish",
+	"Garbodor",
+	"Zorua",
+	"Zoroark",
+	"Minccino",
+	"Cinccino",
+	"Gothita",
+	"Gothorita",
+	"Gothitelle",
+	"Solosis",
+	"Duosion",
+	"Reuniclus",
+	"Ducklett",
+	"Swanna",
+	"Vanillite",
+	"Vanillish",
+	"Vanilluxe",
+	"Deerling",
+	"Sawsbuck",
+	"Emolga",
+	"Karrablast",
+	"Escavalier",
+	"Foongus",
+	"Amoonguss",
+	"Frillish",
+	"Jellicent",
+	"Alomomola",
+	"Joltik",
+	"Galvantula",
+	"Ferroseed",
+	"Ferrothorn",
+	"Klink",
+	"Klang",
+	"Klinklang",
+	"Tynamo",
+	"Eelektrik",
+	"Eelektross",
+	"Elgyem",
+	"Beheeyem",
+	"Litwick",
+	"Lampent",
+	"Chandelure",
+	"Axew",
+	"Fraxure",
+	"Haxorus",
+	"Cubchoo",
+	"Beartic",
+	"Cryogonal",
+	"Shelmet",
+	"Accelgor",
+	"Stunfisk",
+	"Mienfoo",
+	"Mienshao",
+	"Druddigon",
+	"Golett",
+	"Golurk",
+	"Pawniard",
+	"Bisharp",
+	"Bouffalant",
+	"Rufflet",
+	"Braviary",
+	"Vullaby",
+	"Mandibuzz",
+	"Heatmor",
+	"Durant",
+	"Deino",
+	"Zweilous",
+	"Hydreigon",
+	"Larvesta",
+	"Volcarona",
+	"Cobalion",
+	"Terrakion",
+	"Virizion",
+	"Tornadus",
+	"Thundurus",
+	"Reshiram",
+	"Zekrom ",
+	"Landorus",
+	"Kyurem",
+	"Keldeo",
+	"Meloetta",
+	"Genesect",
+	"Chespin",
+	"Quilladin",
+	"Chesnaught",
+	"Fennekin",
+	"Braixen",
+	"Delphox",
+	"Froakie",
+	"Frogadier",
+	"Greninja",
+	"Bunnelby",
+	"Diggersby",
+	"Fletchling",
+	"Fletchinder",
+	"Talonflame",
+	"Scatterbug",
+	"Spewpa",
+	"Vivillon",
+	"Litleo",
+	"Pyroar",
+	"Flabebe",
+	"Floette",
+	"Florges",
+	"Skiddo",
+	"Gogoat",
+	"Pancham",
+	"Pangoro",
+	"Furfrou",
+	"Espurr",
+	"Meowstic",
+	"Honedge",
+	"Doublade",
+	"Aegislash",
+	"Spritzee",
+	"Aromatisse",
+	"Swirlix",
+	"Slurpuff",
+	"Inkay",
+	"Malamar",
+	"Binacle",
+	"Barbaracle",
+	"Skrelp",
+	"Dragalge",
+	"Clauncher",
+	"Clawitzer",
+	"Helioptile",
+	"Heliolisk",
+	"Tyrunt",
+	"Tyrantrum",
+	"Amaura",
+	"Aurorus",
+	"Sylveon",
+	"Hawlucha",
+	"Dedenne",
+	"Carbink",
+	"Goomy",
+	"Sliggoo",
+	"Goodra",
+	"Klefki",
+	"Phantump",
+	"Trevenant",
+	"Pumpkaboo",
+	"Gourgeist",
+	"Bergmite",
+	"Avalugg",
+	"Noibat",
+	"Noivern",
+	"Xerneas",
+	"Yveltal",
+	"Zygarde",
+	"Diancie",
+	"Hoopa",
+	"Volcanion"]
+
+def get_pos_by_name(location_name):
+    geolocator = GoogleV3()
+    loc = geolocator.geocode(location_name)
+    if not loc:
+        return None
+
+    log.info('Your given location: %s', loc.address.encode('utf-8'))
+    log.info('lat/long/alt: %s %s %s', loc.latitude, loc.longitude, loc.altitude)
+
+    return (loc.latitude, loc.longitude, loc.altitude)
+
+def get_cell_ids(lat, long, radius = 10):
+    origin = CellId.from_lat_lng(LatLng.from_degrees(lat, long)).parent(15)
+    walk = [origin.id()]
+    right = origin.next()
+    left = origin.prev()
+
+    # Search around provided radius
+    for i in range(radius):
+        walk.append(right.id())
+        walk.append(left.id())
+        right = right.next()
+        left = left.prev()
+
+    # Return everything
+    return sorted(walk)
+
+def encode(cellid):
+    output = []
+    encoder._VarintEncoder()(output.append, cellid)
+    return ''.join(output)
+
+def init_config():
+    parser = argparse.ArgumentParser()
+    config_file = "config.json"
+
+    # If config file exists, load variables from json
+    load   = {}
+    if os.path.isfile(config_file):
+        with open(config_file) as data:
+            load.update(json.load(data))
+
+    # Read passed in Arguments
+    required = lambda x: x not in load
+    parser.add_argument("-a", "--auth_service", help="Auth Service ('ptc' or 'google')",
+        required=required("auth_service"))
+    parser.add_argument("-u", "--username", help="Username", required=required("username"))
+    parser.add_argument("-p", "--password", help="Password", required=required("password"))
+    parser.add_argument("-l", "--location", help="Location", required=required("location"))
+    parser.add_argument("-d", "--debug", help="Debug Mode", action='store_true')
+    parser.add_argument("-t", "--test", help="Only parse the specified location", action='store_true')
+    parser.set_defaults(DEBUG=False, TEST=False)
+    config = parser.parse_args()
+
+    # Passed in arguments shoud trump
+    for key in config.__dict__:
+        if key in load and config.__dict__[key] == None:
+            config.__dict__[key] = load[key]
+
+    if config.auth_service not in ['ptc', 'google']:
+      log.error("Invalid Auth service specified! ('ptc' or 'google')")
+      return None
+
+    return config
+
+def main():
+    # log settings
+    # log format
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(module)10s] [%(levelname)5s] %(message)s')
+    # log level for http request class
+    logging.getLogger("requests").setLevel(logging.WARNING)
+    # log level for main pgoapi class
+    logging.getLogger("pgoapi").setLevel(logging.INFO)
+    # log level for internal pgoapi class
+    logging.getLogger("rpc_api").setLevel(logging.INFO)
+
+    
+    # starterbot's ID as an environment variable
+    BOT_ID = os.environ.get("BOT_ID")
+    
+
+    config = init_config()
+    if not config:
+        return
+
+    if config.debug:
+        logging.getLogger("requests").setLevel(logging.DEBUG)
+        logging.getLogger("pgoapi").setLevel(logging.DEBUG)
+        logging.getLogger("rpc_api").setLevel(logging.DEBUG)
+
+    position = get_pos_by_name(config.location)
+    if not position:
+        return
+        
+    if config.test:
+        return
+
+    # instantiate pgoapi
+    api = PGoApi()
+
+    # provide player position on the earth
+    api.set_position(*position)
+
+    if not api.login(config.auth_service, config.username, config.password):
+        return
+
+    # chain subrequests (methods) into one RPC call
+        
+
+    # get player profile call
+    # ----------------------
+    response_dict = api.get_player()
+
+    # apparently new dict has binary data in it, so formatting it with this method no longer works, pprint works here but there are other alternatives    
+    # print('Response dictionary: \n\r{}'.format(json.dumps(response_dict, indent=2)))
+     # instantiate Slack & Twilio clients
+    from slackclient import SlackClient
+    slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+    print('Response dictionary: \n\r{}'.format(pprint.PrettyPrinter(indent=4).pformat(response_dict)))
+    if slack_client.rtm_connect():
+            while True:
+                    print slack_client.rtm_read()
+                    find_poi(api, position[0], position[1], slack_client)
+                    time.sleep(60)
+    else:
+        print "Connection Failed, invalid token?"
+
+def find_poi(api, lat, lng, slack_client):
+    poi = {'pokemons': {}, 'forts': []}
+    step_size = 0.000085
+    step_limit = 38
+    coords = generate_spiral(lat, lng, step_size, step_limit)
+    for coord in coords:
+        lat = coord['lat']
+        lng = coord['lng']
+        api.set_position(lat, lng, 0)
+
+        
+        #get_cellid was buggy -> replaced through get_cell_ids from pokecli
+        #timestamp gets computed a different way:
+        from geopy.geocoders import Nominatim
+        geolocator = Nominatim()
+        cell_ids = get_cell_ids(lat, lng)
+        timestamps = [0,] * len(cell_ids)
+        response_dict = api.get_map_objects(latitude = util.f2i(lat), longitude = util.f2i(lng), since_timestamp_ms = timestamps, cell_id = cell_ids)
+        if (response_dict['responses']):
+            if 'status' in response_dict['responses']['GET_MAP_OBJECTS']:
+                if response_dict['responses']['GET_MAP_OBJECTS']['status'] == 1:
+                    for map_cell in response_dict['responses']['GET_MAP_OBJECTS']['map_cells']:
+                        if 'wild_pokemons' in map_cell:
+                            for pokemon in map_cell['wild_pokemons']:
+                                pokekey = get_key_from_pokemon(pokemon)
+                                pokemon['hides_at'] = (time.clock() + pokemon['time_till_hidden_ms']/1000)/60
+                                pokemon['location'] =  geolocator.reverse(repr(pokemon['latitude'])+", "+repr(pokemon['longitude'])).address
+                                poi['pokemons'][pokekey] = pokemon
+                                pokemon['distance'] = distance(lat, lng, pokemon['latitude'], pokemon['longitude'])
+                                #if pokemon['distance'] < 20: 
+        #                            slack_client.api_call(
+        #        "chat.postMessage", channel="#pokemon", text="Pokelert! " + pokemonNames[pokemon['pokemon_data']['pokemon_id'] -1]+" at "+ pokemon['location'] +" hiding for "+ str(pokemon['hides_at']) + " minutes",
+        #        username='pokebot', user="tanushree"
+        #)
+            time.sleep(0.7)
+    # new dict, binary data
+    # print('POI dictionary: \n\r{}'.format(json.dumps(poi, indent=2)))
+    print('POI dictionary: \n\r{}'.format(pprint.PrettyPrinter(indent=4).pformat(poi)))
+    print('Open this in a browser to see the path the spiral search took:')
+    print_gmaps_dbug(coords)
+    
+def distance(lat1, lon1, lat2, lon2):
+    from geopy.distance import great_circle
+    from math import radians, cos, sin, asin, sqrt
+    pointA = (lat1, lon1)
+    pointB = (lat2, lon2)
+    #return great_circle(pointA, pointB).meters
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    m = 6367 * c * 1000
+    return m
+        
+
+def get_key_from_pokemon(pokemon):
+    return '{}-{}'.format(pokemon['spawn_point_id'], pokemon['pokemon_data']['pokemon_id'])
+
+def print_gmaps_dbug(coords):
+    url_string = 'http://maps.googleapis.com/maps/api/staticmap?size=400x400&path='
+    for coord in coords:
+        url_string += '{},{}|'.format(coord['lat'], coord['lng'])
+    print(url_string[:-1])
+
+def generate_spiral(starting_lat, starting_lng, step_size, step_limit):
+    coords = [{'lat': starting_lat, 'lng': starting_lng}]
+    steps,x,y,d,m = 1, 0, 0, 1, 1
+    rlow = 0.0
+    rhigh = 0.0005
+
+    while steps < step_limit:
+        while 2 * x * d < m and steps < step_limit:
+            x = x + d
+            steps += 1
+            lat = x * step_size + starting_lat + random.uniform(rlow, rhigh)
+            lng = y * step_size + starting_lng + random.uniform(rlow, rhigh)
+            coords.append({'lat': lat, 'lng': lng})
+        while 2 * y * d < m and steps < step_limit:
+            y = y + d
+            steps += 1
+            lat = x * step_size + starting_lat + random.uniform(rlow, rhigh)
+            lng = y * step_size + starting_lng + random.uniform(rlow, rhigh)
+            coords.append({'lat': lat, 'lng': lng})
+
+        d = -1 * d
+        m = m + 1
+    return coords
+
+if __name__ == '__main__':
+    main()
